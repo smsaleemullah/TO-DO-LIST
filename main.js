c4acc066e36ae2cf82d463e62a93c58740b10233
smsaleemullah <160390431+smsaleemullah@users.noreply.github.com>
2024-04-01 17:17:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let to_do_list = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'to_do_list',
+            type: 'input',
+            message: "What do you want to add in your to_do_list?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "Do you want to add more ?",
+            default: "false"
+        }
+    ]);
+    to_do_list.push(addTask.to_do_list);
+    condition = addTask.addMore;
+    console.log(to_do_list);
+}
